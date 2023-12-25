85d00e70b4b3eb39f4a504b09d4a27a49e2e1e33
janluak <_>
2023-12-25 11:36:42 +0100

    scripts: pkg/prometheus/promcfg.go
    optimize switch for using relabelings to `> 0`
    
    Fixes #6147

--- pkg/prometheus/promcfg.go
+++ pkg/prometheus/promcfg.go
@@ -2331,7 +2331,7 @@ func (cg *ConfigGenerator) generateScrapeConfig(
 		relabelings = append(relabelings, generateRelabelConfig(labeler.GetRelabelingConfigs(sc.TypeMeta, sc.ObjectMeta, sc.Spec.RelabelConfigs))...)
 	}
 
-	if sc.Spec.JobName != "" || sc.Spec.RelabelConfigs != nil {
+	if len(relabelings) > 0 {
 		cfg = append(cfg, yaml.MapItem{Key: "relabel_configs", Value: relabelings})
 	}
 
